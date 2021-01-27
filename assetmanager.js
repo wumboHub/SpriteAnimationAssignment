92cfc9b69fe67615b4c3b3e233a97e7dd5e63072
wumboHub <75600042+wumboHub@users.noreply.github.com>
2021-01-27 15:02:52 -0800

    January 27
    I've added to and changed some code around to try and get my sprite animation working but nothing is showing up.
    
    I also used it for notes during the lectures so there may be more comments than usual.

--- assetmanager.js
+++ assetmanager.js
@@ -1,43 +1,48 @@
 class AssetManager {
     constructor() {
-        this.successCount = 0;
-        this.errorCount = 0;
-        this.cache = [];
-        this.downloadQueue = [];
+        this.successCount = 0; //tracks success
+        this.errorCount = 0; //tracks errors
+        this.cache = []; //saves downloaed data
+        this.downloadQueue = []; // array of path names
     };
 
     queueDownload(path) {
         console.log("Queueing " + path);
-        this.downloadQueue.push(path);
+        this.downloadQueue.push(path); // stores path names in downloadQueue
     };
 
     isDone() {
+        // checks if queue = success + error
         return this.downloadQueue.length === this.successCount + this.errorCount;
     };
 
     downloadAll(callback) {
+        // if queue is empty, go to callback function
         if (this.downloadQueue.length === 0) setTimeout(callback, 10);
         for (var i = 0; i < this.downloadQueue.length; i++) {
             var img = new Image();
-            var that = this;
+            var that = this; // refers to AssetManager for use in function()
 
+            // print current file path to be downloaded
             var path = this.downloadQueue[i];
             console.log(path);
 
+            // triggers when img is loaded
             img.addEventListener("load", function () {
-                console.log("Loaded " + this.src);
+                console.log("Loaded " + this.src); // prints success
                 that.successCount++;
-                if (that.isDone()) callback();
+                if (that.isDone()) callback(); // when all img's are downloaded, start game
             });
 
+            // triggers if there's an error
             img.addEventListener("error", function () {
-                console.log("Error loading " + this.src);
+                console.log("Error loading " + this.src); // prints error
                 that.errorCount++;
-                if (that.isDone()) callback();
+                if (that.isDone()) callback(); // when all img's are downloaded, start game
             });
 
-            img.src = path;
-            this.cache[path] = img;
+            img.src = path; // triggers download from path and stores in img
+            this.cache[path] = img; // adds new img to cache
         }
     };
 
